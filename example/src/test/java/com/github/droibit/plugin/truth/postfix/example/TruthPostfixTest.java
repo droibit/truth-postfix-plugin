8746df5e9f25451359fc4c0636cfde1f4d8cc3e4
Shinya Kumagai <roomful.rooms@gmail.com>
2016-03-04 18:08:48 +0900

    Created example project

---
+++ example/src/test/java/com/github/droibit/plugin/truth/postfix/example/TruthPostfixTest.java
@@ -0,0 +1,14 @@
+package com.github.droibit.plugin.truth.postfix.example;
+
+import org.junit.Test;
+
+/**
+ * Created by kumagai on 2016/03/04.
+ */
+public class TruthPostfixTest {
+
+    @Test
+    public void test() {
+
+    }
+}
